ae3b67be14c1c4735b3bed910be8d8643b23ae19
Akayush-17 <aaayushsinghh@gmail.com>
2024-09-20 13:04:11 +0530

    feat: added teacher principal grade

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from core.apis import decorators, responses
-from .schema import AssignmentSchema
+from .schema import AssignmentSchema, AssignmentGradeSchema
 from core.models.assignments import Assignment
+from core import db
 
 principal_assignments_resources = Blueprint("principal_assignments_resources", __name__)
 
@@ -12,3 +13,18 @@ def get_all_assignments(p):
     all_assignments = Assignment.get_all_assignments_for_principal()
     all_assignments_dump = AssignmentSchema().dump(all_assignments, many=True)
     return responses.APIResponse.respond(data=all_assignments_dump)
+
+@principal_assignments_resources.route("/assignments/grade", methods=["POST"],strict_slashes=False )
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_or_regrade_assigments(p, incoming_payload):
+    """Principal: Grades Or Regrade any Assignment"""
+    assigment_grade_payload =  AssignmentGradeSchema().load(incoming_payload)
+    graded_assigment = Assignment.mark_grade(
+        _id = assigment_grade_payload.id,
+        grade = assigment_grade_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assigment)
+    return responses.APIResponse.respond(data=graded_assignment_dump)   
